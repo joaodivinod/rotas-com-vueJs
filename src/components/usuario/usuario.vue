01fbcfd1fb7c81b96c0ed8a4b8965f0cc671b772
João Divino <joaodivinod@gmail.com>
2021-03-28 21:08:34 -0300

    Navegando de forma programatica

--- src/components/usuario/usuario.vue
+++ src/components/usuario/usuario.vue
@@ -1,13 +1,20 @@
 <template>
 <div class="usuario">
     <h2>Usuários</h2>
+    <button sucesso @click="irParaInicio">Voltar</button>
 </div>
 </template>
 
 <script>
 export default {
-    name: "usuario"
+    name: "usuario",
+    methods:{
+        irParaInicio(){
+            this.$router.push('/')
+        }
+    }
 }
+
 </script>
 
 <style scoped>
